8ea4bee7934c4541550be2a5989c50c8d013839a
Jonathan <93867018+DarkSkull93@users.noreply.github.com>
2021-12-28 12:36:32 +0530

    Update userbotjoin.py

--- program/userbotjoin.py
+++ program/userbotjoin.py
@@ -1,5 +1,5 @@
 import asyncio
-from driver.doozy import user
+from driver.veez import user
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
